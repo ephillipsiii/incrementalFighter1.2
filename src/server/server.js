39a576211d14e5f155da54da9644bcc3aa9bf967
Edford Phillips <ed.d.phillips.iii@gmail.com>
2018-07-09 21:23:04 -0600

    fleshed out server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,24 @@
-var express = require("express");
-var app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.get('/', function(req,res){
-    res.send("hello world")
-})
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
 
-app.listen(3000)
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
