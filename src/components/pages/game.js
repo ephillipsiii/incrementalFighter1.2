7f92c31623120fd8189c7555bc713cbeda57e908
Dylan Thomas <azelthedemon@gmail.com>
2018-07-09 20:26:31 -0600

    worked on filling background image, basic image placed on gear.js

--- src/components/pages/game.js
+++ src/components/pages/game.js
@@ -2,7 +2,7 @@ import React from 'react';
 import player from '../../images/gargoyle_male.png'
 import enemy from '../../images/death_knight.png'
 import floor from '../../images/tomb_1_old.png'
-import {mainWeapon} from './gear'
+import { mainWeapon } from './gear'
 
 const Game = props => (
     <div style={{
@@ -10,33 +10,43 @@ const Game = props => (
     }}>
         <div
             style={{
-                width:'50%',
-                height:'100%',
-                position:'fixed',
-                right:0
+                width: '50%',
+                height: '100%',
+                position: 'fixed',
+                right: 0,
+                backgroundImage: `url(${floor})`
             }}
             onClick={() => props.attack(mainWeapon.dmg)}
         >
             <p style={{
-                color:'white'
+                color: 'white'
             }}>{props.enemyHealth}</p>
             <img src={enemy} alt='Enemy' style={{
                 height: '100px',
                 width: '100px'
-            }}/>
+            }} />
         </div>
-        <div>
-            <p style={{
-                textAlign: 'left'
-            }}>Action Log</p>
-        </div>
-        <div>
-            <img src={player} alt='Player' style={{
-                position: 'fixed',
-                left: 0, 
-                bottom: 0,
-                width: '100px',
-                height: '100px' }} />
+        <div style={{
+            backgroundImage:   `url(${floor})`
+        }}>
+            <div>
+                <p style={{
+                    color: 'white',
+                    textAlign: 'left'
+                }}>Action Log</p>
+            </div>
+            <div
+                style={{
+                    backgroundImage: `url(${floor})`
+                }}>
+                <img src={player} alt='Player' style={{
+                    position: 'fixed',
+                    left: 0,
+                    bottom: 0,
+                    width: '100px',
+                    height: '100px',
+                }} />
+            </div>
         </div>
     </div>
 );
