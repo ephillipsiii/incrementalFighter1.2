646d38cfbd19eb97c2a38994428e5f58028879b0
Dylan Thomas <azelthedemon@gmail.com>
2018-07-11 19:06:01 -0600

    fixing styling background images;

--- client/src/components/pages/game.js
+++ client/src/components/pages/game.js
@@ -8,59 +8,77 @@ const Game = props => (
     <div style={{
         backgroundImage: `url(${floor})`
     }}>
-
         <div
             style={{
+                position: 'fixed',
+                left: 0,
+                height: '100%',
+                width: '50%',
                 backgroundImage: `url(${floor})`
             }}
         >
-            <p style={{
-                color: 'white',
-                textAlign: 'left'
-            }}>Action Log</p>
-            <p style={{
-                color: 'white',
-                textAlign: 'left',
-                margin: 0
+            <div style={{
+                backgroundImage: `url(${floor})`
             }}>
-                Enemies Killed:
+                <div
+                    style={{}}
+                >
+                    <p style={{
+                        color: 'white',
+                        textAlign: 'left'
+                    }}>Action Log</p>
+                    <p style={{
+                        color: 'white',
+                        textAlign: 'left',
+                        margin: 0
+                    }}>
+                        Enemies Killed:
                     {props.enemiesKilled}
-            </p>
-        </div>
+                    </p>
+                </div>
 
-        <div
-            style={{
-                width: '100%',
-                height: '100%',
+
+
+                <div style={{
+                    backgroundImage: `url(${floor})`
+                }}>
+                    <img src={player} alt='Player' style={{
+                        position: 'fixed',
+                        left: 0,
+                        bottom: 0,
+                        width: '100px',
+                        height: '100px',
+                    }} />
+                </div>
+            </div>
+
+            <div style={{
                 position: 'fixed',
+                height: '100%',
+                width:'50%',
                 right: 0,
                 backgroundImage: `url(${floor})`
             }}>
-            <p style={{
-                textAlign: 'right',
-                color: 'white'
-            }}>{props.enemyHealth}</p>
-            <img src={enemy} alt='Enemy' style={{
-                position:'fixed',
-                right: 0,
-                height: '100px',
-                width: '100px'
-            }}
-                onClick={() => props.attack(mainWeapon.dmg)}
-            />
-            
-        <div>
-            <img src={player} alt='Player' style={{
-                position: 'fixed',
-                left: 0,
-                bottom: 0,
-                width: '100px',
-                height: '100px',
-            }} />
-        </div>
-        </div>
+                <div
+                    style={{
+                    }}>
+                    <p style={{
+                        textAlign: 'right',
+                        color: 'white'
+                    }}>{props.enemyHealth}</p>
+                    <img src={enemy} alt='Enemy' style={{
+                        position: 'fixed',
+                        right: 0,
+                        height: '100px',
+                        width: '100px'
+                    }}
+                        onClick={() => props.attack(mainWeapon.dmg)}
+                    />
 
 
+                </div>
+            </div>
+        </div>
     </div>
 );
 
