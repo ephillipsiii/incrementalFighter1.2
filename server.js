526f1395664be299111decebbca405ee38efa48d
unknown <matthewhknetsch@gmail.com>
2018-07-11 21:19:47 -0600

    saftey

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// Serve up static assets (usually on heroku)
-app.use(express.static("client/build"));
+
 
 // Add routes, both API and view
 // app.use(routes);
