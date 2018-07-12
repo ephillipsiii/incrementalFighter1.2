efca0dbb7810c56d8311b5b0d56f22bc53295cf4
unknown <matthewhknetsch@gmail.com>
2018-07-12 15:48:18 -0600

    added more schemas

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 // app.use(routes);
 
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/incDatabase");
 
 // Start the API server
 app.listen(PORT, function() {
