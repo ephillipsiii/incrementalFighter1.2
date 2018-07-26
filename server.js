bab012e4800fd7a98d7184eb6d6eee5d63272ede
Dylan Thomas <azelthedemon@gmail.com>
2018-07-25 19:30:10 -0600

    still trying to fix heroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_PURPLE_URI || "mongodb://localhost/incDatabase");
+mongoose.connect(process.env.MONGODB_PURPLE_URI || "mongodb://localhost/27017");
 
 // Start the API server
 app.listen(PORT, function() {
